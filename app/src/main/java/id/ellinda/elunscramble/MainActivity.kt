df570b380b25b3d6cefe6405ca14914f2b382f05
ellindaa <ellinda.ellinda@students.utdi.ac.id>
2024-11-20 21:29:14 +0700

    initial commit

---
+++ app/src/main/java/id/ellinda/elunscramble/MainActivity.kt
@@ -0,0 +1,27 @@
+package id.ellinda.elunscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import id.ellinda.elunscramble.ui.GameScreen
+import id.ellinda.elunscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
